4fae565cb6a01c5920f4674c76f79b312d0dbb49
harshvardhan258 <34251441+harshvardhan258@users.noreply.github.com>
2022-07-04 01:53:14 +0530

    Update refactor.js

--- refactor.js
+++ refactor.js
@@ -14,3 +14,21 @@ function binarySearch(arr,no){
     }
     return -1;
 }
+
+
+//Multiple Pointers - isSubsequence
+//isSubsequence is a function which takes in two strings and checks whether the characters
+//in 1 string sorm subsequence of characters in the 2 string
+
+//Recussive Method
+//example : isSubsequence('sing','string')  ===> true
+//          isSubsequence('abc','acb')  ===> false (orders matter)   
+
+function isSubsequence(str1,str2,m=str1.length,n=str2.length) {
+  // good luck. Add any arguments you deem necessary.
+  if(m === 0) return true;
+  if(n === 0) return false;
+  
+  if(str1[m-1] === str2[n-1]) return isSubsequence(str1,str2,m-1,n-1);
+  return isSubsequence(str1,str2,m,n-1);
+}
