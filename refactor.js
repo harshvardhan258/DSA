42e6662122b777db48f01318b4b2fe9af46cd0a7
harshvardhan258 <34251441+harshvardhan258@users.noreply.github.com>
2022-07-06 14:10:10 +0530

    Update refactor.js

--- refactor.js
+++ refactor.js
@@ -32,3 +32,55 @@ function isSubsequence(str1,str2,m=str1.length,n=str2.length) {
   if(str1[m-1] === str2[n-1]) return isSubsequence(str1,str2,m-1,n-1);
   return isSubsequence(str1,str2,m,n-1);
 }
+
+// sliding Window - maxSubArraySum
+//Given an array of integers and a number, write a function called maxSubArraySum
+//, which finds the maximum sum of a subarray with the length of the number passed to the function. 
+//
+//Note that a subarray must consist of consecutive  elements from the original array. In the first example below, [100, 200, 300] is a subarray of the original array, but [100, 300] is not.
+
+//maxSubarraySum([100,200,300,400],2) ----> 700
+//maxSubarraySum([1,4,2,10,23,3,1,0,20],4) ---->39
+
+//Solution 1   Time complexity is -> O(n*k)
+function maxSubArray(ar,no){
+    let maxSum=0;
+
+    if(ar.length<no) return null;
+
+    for(let i=0;i<ar.length;i++){
+        let temp=0;
+        for(let j=0;j<no;j++){
+            temp+=ar[i+j];
+        }
+
+        if(temp>maxSum) {
+            maxSum=temp;
+        }
+    }
+
+    return maxSum; 
+}
+
+//Solution 2  Time complexity is -> O(n)
+
+function maxSubarraySum(ar,no){
+    let maxSum=0;
+    let currentSum=0;
+    
+    if(ar.length<no) return null;
+
+    for(let i=0;i<no;i++){
+        currentSum+=ar[i];
+    }
+
+    for(let i=no;i<ar.length;i++){
+        currentSum= currentSum +ar[i]-ar[i-no];
+
+        if(currentSum>maxSum){
+            maxSum=currentSum;}
+    }
+    return maxSum; 
+}
+
+
