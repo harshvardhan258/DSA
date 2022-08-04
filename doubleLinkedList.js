85368b8440e6e44eddeac9eb76b947d1d6438d22
harshvardhan258 <34251441+harshvardhan258@users.noreply.github.com>
2022-08-04 12:08:07 +0530

    Update doubleLinkedList.js

--- doubleLinkedList.js
+++ doubleLinkedList.js
@@ -44,6 +44,58 @@ class DoublyLinkedList{
         this.length--;
         return poppedNode;
     }
+
+    shift(){
+        if(this.length===0) return undefined;
+        let shiftNode=this.head;
+        if(this.length===1){
+            this.head=null;
+            this.tail=null;
+        }
+        if(this.length>1){
+            this.head=shiftNode.next;
+            this.head.prev=null;
+            shiftNode.next=null;
+        }
+        this.length--;
+        return shiftNode;
+    }
+
+    unshift(val){
+        let newNode=new Node(val);
+        if(this.length === 0){
+            this.head=newNode;
+            this.tail=newNode;
+        } else {
+            this.head.prev=newNode;
+            newNode.next=this.head;
+            this.head=newNode;
+        }
+        this.length++;
+        return this;
+    }
+
+    get(idx){
+        if(idx<0||idx>=this.length) return undefined;
+        let mid=Math.floor(this.length/2);
+        if(idx>mid){
+            let count=this.length-1;
+            let temp=this.tail;
+            while(count!==idx){
+                temp=temp.prev;
+                count--;
+            }
+            return temp;
+        } else {
+            let count=0
+            let temp=this.head;
+            while(count!==idx){
+                temp=temp.next;
+                count++;
+            }
+            return temp;
+        }
+    }
 }
 
 let dlist=new DoublyLinkedList();
