0d6b6cf4ea67da5e4ed95d3bf9a5164bb89d4609
harshvardhan258 <34251441+harshvardhan258@users.noreply.github.com>
2022-08-17 13:58:32 +0530

    Update TreeTraversal.js

--- TreeTraversal.js
+++ TreeTraversal.js
@@ -133,7 +133,10 @@ Trees are non linear data sructure that contain a root and child nodes
 
 Binary Trees can have values of any type, but at most two children for each parent.
 
-BST(Binary Search Trees) are a more specific version of binary trees where every node to the left of a parent is less than it's value and every node to the right is greater.
+BST(Binary Search Trees) are a more specific version of binary trees where every node to the left of a parent is less 
+than it's value and every node to the right is greater.
+
+We can search in trees using BFS and DFS.
 
 ---> when to use
 
